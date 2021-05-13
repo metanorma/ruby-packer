0905749d49b049de22412141b7e4882f08f4cba9
Alexey Morozov <alexey.morozov.is@gmail.com>
2021-05-13 15:32:13 +0300

    Build at GHA

---
+++ bin/build-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+RUBY_VERSION=$(cat .ruby-version)
+docker run -v $(pwd):/rubyc -w /rubyc ruby:$RUBY_VERSION bash -c '
+apt-get update -y && apt-get install -y squashfs-tools bison flex texinfo;
+echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections;
+export TEMP_DIR="$(mktemp -d .rubyc-build.XXXXXX)";
+cp -r * $TEMP_DIR;
+bin/rubyc --clean-tmpdir -r "$TEMP_DIR" -o rubyc-linux-x64 "$TEMP_DIR/bin/rubyc"
+chown -R 1000:1000 .rubyc-build.*
+chown 1000:1000 rubyc-linux-x64
+'
