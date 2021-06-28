e897d0c75ffb1d278f46788b700498fac6f9a8d2
Alexey Morozov <alexey.morozov.is@gmail.com>
2021-06-28 16:54:36 +0300

    Move bin/build-*.sh to GHA and Makefile

--- bin/build-linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export TEMP_DIR="$(mktemp -d .rubyc-build.XXXXXX)";
-cp -r * $TEMP_DIR;
-bin/rubyc --clean-tmpdir -r "$TEMP_DIR" -o rubyc-linux-x64 "$TEMP_DIR/bin/rubyc"
-strip rubyc-linux-x64
