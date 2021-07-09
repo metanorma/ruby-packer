4ae327e137d3d5da1f49ccc7aea8d99c2af96afd
Alexey Morozov <alexey.morozov.is@gmail.com>
2021-07-09 12:59:34 +0300

    Bump version to v0.6.1

--- lib/compiler/constants.rb
+++ lib/compiler/constants.rb
@@ -4,7 +4,7 @@
 # For full terms see the included LICENSE file
 
 class Compiler
-  VERSION = '0.6.0'
+  VERSION = '0.6.1'
   PRJ_ROOT = File.expand_path('../../..', __FILE__)
   VENDOR_DIR = File.expand_path('vendor', PRJ_ROOT)
   MEMFS = '/__ruby_packer_memfs__'
